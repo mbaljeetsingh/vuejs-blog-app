705e76575806d5949fefea5251f27fa958c834e5
mbaljeetsingh <bsinghs.007@gmail.com>
2017-01-26 20:44:32 +0530

    Added vue-resouce & logic

--- src/components/Blog.vue
+++ src/components/Blog.vue
@@ -35,12 +35,12 @@
 
     <hr>
     <div class="row">
-    <div class="col-sm-6 col-md-4" v-for="post in posts">
+    <div class="col-sm-6 col-md-4" v-for="post in posts" style="height: 400px;">
       <div class="thumbnail">
         <img src="http://placehold.it/350x150">
         <div class="caption">
           <h3>{{post.title | uppercase}}</h3>
-          <p>{{post.body}}</p>
+          <!-- <p>{{post.body}}</p> -->
           <p><a href="#" class="btn btn-primary" role="button">Read More</a> <a href="#" class="btn btn-danger" role="button" v-on:click="deletePost(post)">Delete</a></p>
         </div>
       </div>
@@ -91,6 +91,12 @@ export default {
 
       this.newPost = {}
     }
+  },
+  created: function () {
+    this.$http.get('https://jsonplaceholder.typicode.com/posts').then(function (response) {
+      // console.log(response.data);
+      this.posts = response.data
+    })
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import VueResource from 'vue-resource'
+
+Vue.use(VueResource)
 
 Vue.filter('uppercase', function (value) {
   return value.toUpperCase()
