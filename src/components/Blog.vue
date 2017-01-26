1ba92c8177e41c3dfb45ad12845b3ebd056d7dcb
mbaljeetsingh <bsinghs.007@gmail.com>
2017-01-26 20:22:16 +0530

    Add new post

--- src/components/Blog.vue
+++ src/components/Blog.vue
@@ -1,6 +1,38 @@
 <template>
   <div class='blog container'>
-    <h2>{{title}}</h2>
+    <h2>{{title}}</h2> 
+
+
+    <!-- Trigger the modal add post with a button -->
+    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#addNewPost">New Post</button>
+    <!-- Add Post -->
+    <div id="addNewPost" class="modal fade" role="dialog">
+      <div class="modal-dialog">
+        <!-- Add Post Content-->
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal">&times;</button>
+            <h4 class="modal-title">Add New Post</h4>
+          </div>
+          <div class="modal-body">
+              <div class="form-group">
+                <label for="title">Title:</label>
+                <input type="title" class="form-control" id="title" v-model="newPost.title">
+              </div>
+              <div class="form-group">
+                <label for="body">Content:</label>
+                <textarea class="form-control" rows="5" id="body" v-model="newPost.body"></textarea>
+              </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal" v-on:click="AddPost()">Add</button>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+
     <hr>
     <div class="row">
     <div class="col-sm-6 col-md-4" v-for="post in posts">
@@ -23,6 +55,7 @@ export default {
   data () {
     return {
       title: 'Blog App',
+      newPost: {},
       posts: [{
         'userId': 1,
         'id': 1,
@@ -47,6 +80,16 @@ export default {
     deletePost: function (post) {
       console.log(post)
       this.posts.splice(this.posts.indexOf(post), 1)
+    },
+    AddPost: function () {
+      this.posts.push({
+        'userId': 1,
+        'id': this.posts.length + 1,
+        'title': this.newPost.title,
+        'body': this.newPost.body
+      })
+
+      this.newPost = {}
     }
   }
 }
